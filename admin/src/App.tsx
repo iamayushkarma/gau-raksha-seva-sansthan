8b418ef876142be72c62aabdbe08d4f6b4f5c8a5
Ayush Karma <ayushkarma.dev@gmail.com>
2026-03-10 11:01:52 +0530

    fix: redirect to login page when visits to admin page

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -13,6 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/admin/login" replace />} />
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route
           path="/admin"
